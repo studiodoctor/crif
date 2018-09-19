56620e3cc7352800a5f6a237fb68f26fe2da4f55
Android Developer <manpreetsingh2991@gmail.com>
2018-09-19 16:37:19 +0530

    Initial commit

---
+++ app/src/main/java/crif/android/sdk/Master.java
@@ -0,0 +1,13 @@
+package crif.android.sdk;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Master extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_master);
+    }
+}
---
+++ crif-library/src/main/java/com/crif/android/crif_library/CRIFData.java
@@ -0,0 +1,13 @@
+package com.crif.android.crif_library;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class CRIFData {
+
+    public static void UPLOAD_DATA(Context c, String message){
+
+        Toast.makeText(c,message,Toast.LENGTH_SHORT).show();
+
+    }
+}
