209bfac95b6ae42a13a5e9ad16d281e199226068
Android Developer <manpreetsingh2991@gmail.com>
2018-09-20 15:40:00 +0530

    Download Service

--- crif-library/src/main/java/com/crif/android/crif_library/CRIFData.java
+++ crif-library/src/main/java/com/crif/android/crif_library/CRIFData.java
@@ -1,16 +1,19 @@
 package com.crif.android.crif_library;
 
 import android.content.Context;
+import android.content.Intent;
 import android.widget.Toast;
 
 public class CRIFData {
-
-    private Context context;
 
     public static void UPLOAD_DATA(Context context, String message) {
 
         Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
 
+        Intent playerservice = new Intent();
+        playerservice.setAction("MYSERVICE");
+        context.startService(playerservice);
+//       context.bindService(playerservice,conn , Service.BIND_AUTO_CREATE);
 
     }
 }
