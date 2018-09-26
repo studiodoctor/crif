05f08480e4a9b232e61756c686ae0fb57f6b55e3
Android Developer <manpreetsingh2991@gmail.com>
2018-09-26 17:32:03 +0530

    Service Update5

--- crif-library/src/main/java/com/crif/android/crif_library/CRIFData.java
+++ crif-library/src/main/java/com/crif/android/crif_library/CRIFData.java
@@ -8,66 +8,10 @@ public class CRIFData {
     public static DownloadService localService;
     public static boolean isBound = false;
 
-    public static void UPLOAD_DATA(Context context, String message) {
-
-        //  Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
-
-
-//        Intent playerservice = new Intent();
-//        playerservice.setAction("com.crif.android.crif_library.DownloadService");
-//
-//        Intent explicitIntent = convertImplicitIntentToExplicitIntent(playerservice, context);
-//        if(explicitIntent != null){
-//            context.startService(explicitIntent);
-//        }
-        // context.bindService(playerservice,connection , Service.BIND_AUTO_CREATE);
-
-//        Intent intent = new Intent("com.crif.android.crif_library.DownloadService");
-//        intent.setPackage("com.crif.android.crif_library");
-//        context.startService(intent);
+    public static void UPLOAD_DATA(Context context, String id) {
 
         Intent intent = new Intent(Intent.ACTION_SYNC, null, context, DownloadService.class);
+        intent.putExtra("Id", id);
         context.startService(intent);
     }
-
-//    public static Intent createExplicitFromImplicitIntent(Context context, Intent implicitIntent) {
-//        //Retrieve all services that can match the given intent
-//        PackageManager pm = context.getPackageManager();
-//        List<ResolveInfo> resolveInfo = pm.queryIntentServices(implicitIntent, 0);
-//
-//        //Make sure only one match was found
-//        if (resolveInfo == null || resolveInfo.size() != 1) {
-//            return null;
-//        }
-//
-//        //Get component info and create ComponentName
-//        ResolveInfo serviceInfo = resolveInfo.get(0);
-//        String packageName = serviceInfo.serviceInfo.packageName;
-//        String className = serviceInfo.serviceInfo.name;
-//        ComponentName component = new ComponentName(packageName, className);
-//
-//        //Create a new intent. Use the old one for extras and such reuse
-//        Intent explicitIntent = new Intent(implicitIntent);
-//
-//        //Set the component to be explicit
-//        explicitIntent.setComponent(component);
-//
-//        return explicitIntent;
-//    }
-//
-//    public static ServiceConnection connection = new ServiceConnection() {
-//        @Override
-//        public void onServiceConnected(ComponentName className, IBinder service) {
-//            DownloadService.LocalBinder binder = (DownloadService.LocalBinder) service;
-//            localService = binder.getService();
-//            isBound = true;
-//
-//        }
-//
-//        @Override
-//        public void onServiceDisconnected(ComponentName arg0) {
-//            isBound = false;
-//        }
-//    };
-
 }
--- crif-library/src/main/java/com/crif/android/crif_library/DownloadService.java
+++ crif-library/src/main/java/com/crif/android/crif_library/DownloadService.java
@@ -1,11 +1,32 @@
 package com.crif.android.crif_library;
 
 import android.app.IntentService;
+import android.content.Context;
 import android.content.Intent;
+import android.location.Address;
+import android.location.Geocoder;
+import android.os.AsyncTask;
 import android.support.annotation.Nullable;
+import android.text.TextUtils;
+import android.util.Log;
 import android.widget.Toast;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.List;
+import java.util.Locale;
+
 public class DownloadService extends IntentService {
+
+    private String HOME_URL = "http://18.191.145.152/api/";
+    private String LOCATION_URL = "Locationtbs/PostLocationtb";
 
     public DownloadService() {
         super("MyService");
@@ -13,6 +34,158 @@ public class DownloadService extends IntentService {
 
     @Override
     protected void onHandleIntent(@Nullable Intent intent) {
-        Toast.makeText(this, "Service Entered", Toast.LENGTH_SHORT).show();
+
+        final String id = intent.getStringExtra("Id");
+
+        SingleShotLocationProvider.requestSingleUpdate(getApplicationContext(),
+                new SingleShotLocationProvider.LocationCallback() {
+                    @Override
+                    public void onNewLocationAvailable(SingleShotLocationProvider.GPSCoordinates location) {
+                        Log.d("Location", "my location is " + location.toString());
+                        getAddress(getApplicationContext(),location.latitude, location.longitude,id);
+                    }
+                });
+
+        Toast.makeText(getApplicationContext(), "Service Entered", Toast.LENGTH_SHORT).show();
     }
+
+    public void getAddress(Context context,float latitude, float longitude,String userId) {
+        Address locationAddress;
+
+        locationAddress = getStringAddress(latitude, longitude,context);
+
+        if (locationAddress != null) {
+
+            String address = locationAddress.getAddressLine(0);
+            String address1 = locationAddress.getAddressLine(1);
+            String city = locationAddress.getLocality();
+            String state = locationAddress.getAdminArea();
+            String country = locationAddress.getCountryName();
+            String postalCode = locationAddress.getPostalCode();
+
+
+            String currentLocation;
+
+            if (!TextUtils.isEmpty(address)) {
+                currentLocation = address;
+
+                if (!TextUtils.isEmpty(address1))
+                    currentLocation += "," + address1;
+
+                if (!TextUtils.isEmpty(city)) {
+                    currentLocation += "," + city;
+
+                    if (!TextUtils.isEmpty(postalCode))
+                        currentLocation += " - " + postalCode;
+                } else {
+                    if (!TextUtils.isEmpty(postalCode))
+                        currentLocation += "," + postalCode;
+                }
+
+                if (!TextUtils.isEmpty(state))
+                    currentLocation += "," + state;
+
+                if (!TextUtils.isEmpty(country))
+                    currentLocation += "," + country;
+
+                JSONObject jsonToSend = new JSONObject();
+                try {
+                    jsonToSend.put("UserId", userId);
+                    jsonToSend.put("Location", currentLocation);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+                new UploadData(HOME_URL + LOCATION_URL, jsonToSend).execute();
+//                tvAddress.setText(currentLocation);
+            }
+        }
+    }
+
+    public Address getStringAddress(double latitude, double longitude, Context context) {
+        Geocoder geocoder;
+        List<Address> addresses;
+        geocoder = new Geocoder(context, Locale.getDefault());
+
+        try {
+            addresses = geocoder.getFromLocation(latitude, longitude, 1); // Here 1 represent max location result to returned, by documents it recommended 1 to 5
+            return addresses.get(0);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+
+    }
+
+    public class UploadData extends AsyncTask<Void, Void, JSONObject> {
+        private String URL;
+        private JSONObject jsonObjSend;
+
+        public UploadData(String URL, JSONObject jsonObjSend) {
+            this.URL = URL;
+            this.jsonObjSend = jsonObjSend;
+        }
+
+        @Override
+        protected void onPreExecute() {
+
+        }
+
+        @Override
+        protected JSONObject doInBackground(Void... params) {
+            JSONObject jsonObjRecv = new JSONObject();
+            java.net.URL url = null;
+            HttpURLConnection con = null;
+            BufferedReader in = null;
+            String inputLine;
+            String result;
+            try {
+                url = new URL(URL);
+                String jsonString = "";
+                jsonString = jsonObjSend.toString();
+
+                Log.i("TAG", "URL:" + url.toString());
+                con = (HttpURLConnection) url.openConnection();
+                con.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+                con.setDoOutput(true);
+                con.setConnectTimeout(2000);
+                con.setDoInput(true);
+                con.setRequestMethod("POST");
+                OutputStream os = con.getOutputStream();
+                os.write(jsonString.getBytes("UTF-8"));
+                os.close();
+
+                InputStreamReader streamReader = new
+                        InputStreamReader(con.getInputStream());
+                //Create a new buffered reader and String Builder
+                BufferedReader reader = new BufferedReader(streamReader);
+                StringBuilder stringBuilder = new StringBuilder();
+                //Check if the line we are reading is not null
+                while ((inputLine = reader.readLine()) != null) {
+                    stringBuilder.append(inputLine);
+                }
+                //Close our InputStream and Buffered reader
+                reader.close();
+                streamReader.close();
+                //Set our result equal to our stringBuilder
+                result = stringBuilder.toString();
+                jsonObjRecv = new JSONObject();
+
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return jsonObjRecv;
+        }
+
+        protected void onPostExecute(JSONObject result) {
+
+          //  Toast.makeText(MainActivity.this, "Data uploaded !!", Toast.LENGTH_SHORT).show();
+        }
+
+
+    }
+
 }
